1675ec3c8185282ab7e81c710f2e34ad625428a8
Boxi Xia <bx2150@columbia.edu>
2021-11-16 12:19:13 -0500

    process markermap

--- aruco_print_dictionary.cpp
+++ aruco_print_dictionary.cpp
@@ -33,6 +33,7 @@ or implied, of Rafael Muñoz Salinas.
 #include <opencv2/imgproc/imgproc.hpp>
 #include <string>
 #include <clipp.h>
+#include <filesystem>
 
 using namespace std;
 
@@ -40,17 +41,17 @@ int main(int argc, char** argv)
 {
 	try
 	{
-		int pixSize = 75;
+		int bit_size = 150;
 		string outdir;
-		string dictionaryString;
+		string dictionaryString = "ARUCO_MIP_25h7";
 		bool help = false;
 		bool enclosed_corners = false;
 		{
 			using namespace clipp;//https://github.com/muellan/clipp
 			auto cli = ((
-				value("outdir", outdir).doc("set output directory (it must exist)"),
-				value("dict", dictionaryString).doc("set dictionary"),
-				option("-s").doc("set bit_image_size: 75 default") & value("bit_image_size", pixSize),
+				option("-f").doc("set output directory (it must exist)") & value("outdir", outdir),
+				option("-d").doc("set dictionary")& value("dict", dictionaryString),
+				option("-s").doc("set bit_image_size: 150 default") & value("bit_image_size", bit_size),
 				option("-e").doc("set enclosed_corners for subpixel refinement").set(enclosed_corners,true)
 				) | option("-h", "--help").doc("show help").set(help, true)
 				);
@@ -65,16 +66,39 @@ int main(int argc, char** argv)
 		aruco::Dictionary dict = aruco::Dictionary::load(dictionaryString);
 		string dict_name = dict.getName();
 		std::transform(dict_name.begin(), dict_name.end(), dict_name.begin(), ::tolower);
-		//
+		
+		// create folder
+		if (outdir.empty()) { outdir = "./" + dictionaryString; }
+		namespace fs = std::filesystem; // In C++17 use std::filesystem.
+		std::error_code ec;
+		bool success = fs::create_directories(outdir, ec);
+		if (!success) {std::cout << ec.message() << std::endl;}
+
+		// write text id
+		const int nBitsSquared = static_cast<int>(std::sqrt(dict.nbits()));
+		const int A = bit_size * (2 + nBitsSquared);
+		float ax = static_cast<float>(A) / 100.f;
+
 		for (auto m : dict.getMapCode())
 		{
-			string number = std::to_string(m.second);
-			while (number.size() != 5)
-				number = "0" + number;
+			int id = m.second;
+			string id_str = std::to_string(id);
+			while (id_str.size() != 5)
+				id_str = "0" + id_str;
 			stringstream name;
-			name << argv[1] << "/" + dict_name + "_" << number << ".png";
+			name << outdir << "/" + dict_name + "_" << id_str << ".png";
 			cout << name.str() << endl;
-			cv::imwrite(name.str(), dict.getMarkerImage_id(m.second, pixSize, true, enclosed_corners, true, true));
+			cv::Mat img = dict.getMarkerImage_id(m.second, bit_size, true, enclosed_corners, true, true);
+
+			// write text id
+			int linew = 1 + (img.rows / 500);
+			char idcad[30];
+			sprintf(idcad, "#%d", id);
+			cv::putText(img, idcad, cv::Point(0, img.rows - img.rows / 40), cv::FONT_HERSHEY_COMPLEX, ax * 0.3f, cv::Scalar::all(220), linew);
+
+			cv::imwrite(name.str(), img);
+		
+			//break;
 		}
 	}
 	catch (std::exception& ex)
---
+++ py/process_markermap.py
@@ -0,0 +1,87 @@
+"""
+helper to move the center of mass position to origin
+"""
+import open3d as o3d
+import open3d.visualization.gui as gui
+
+import numpy as np
+
+import yaml
+import re
+import argparse
+
+    
+class marker:
+    def __init__(self, config):
+        self.id = config['id']
+        self.corners = np.asarray(config['corners'],dtype=float)         
+    def text3d(self,vis):
+        com_pos = self.corners.mean(axis=0)
+        vis.add_3d_label(com_pos, f"{self.id}")
+    def lineset(self):
+        return o3d.geometry.LineSet(o3d.utility.Vector3dVector(self.corners),
+                             o3d.utility.Vector2iVector(np.array([[0,1],[1,2],[2,3],[3,0]])))
+    
+    
+def arucoTest(path,gui=True):
+    # path = "..\\board_config.yml"
+    # path = "..\\markerset.yml"
+    # path = "..\\markerset.yml"
+
+    with open(path,mode='r') as file:
+        file_str = file.read()
+        file_str_fixed = re.sub(":+ |:",": ",file_str)
+    # check if file need fixing
+    # opencv does not exactly follow the yaml convention,
+    # so may need to replace ":" with ": "
+    if file_str != file_str_fixed:
+        # fix the string
+        with open(path,mode='w') as file:
+            file.write(file_str_fixed)
+    config = yaml.load(file_str_fixed,Loader=yaml.FullLoader)
+                
+        
+    markers = [marker(m) for m in config["aruco_bc_markers"]]
+    
+    # move marker com to origin
+    com_pos = np.vstack([m.corners for m in markers]).mean(axis=0)
+    for m in markers:
+        m.corners -= com_pos
+
+    if gui:
+        app = gui.Application.instance
+        app.initialize()
+        vis = o3d.visualization.O3DVisualizer("Open3D - 3D Text", 1024, 768)
+        
+        for m in markers:
+            m.text3d(vis)
+            vis.add_geometry(f"{m.id}",m.lineset())
+            
+        vis.show_settings = True
+        vis.show_axes = True
+        vis.line_width = 5
+        vis.reset_camera_to_default()
+
+        app.add_window(vis)
+        app.run() 
+
+    # save to the config
+    for k in range(len(markers)):
+        config["aruco_bc_markers"][k]['corners'] = markers[k].corners.tolist()
+    # opencv does not support higher yaml standard, so need to write custom composer
+    header = "%YAML 1.0\n---\n"
+    body = "\n".join([f"{key}: {item}" if key!= 'aruco_bc_markers' else f"{key}:" for key, item in config.items()])+"\n"
+    marker_string = "\n".join([f"   - {m}" for m in config['aruco_bc_markers']])
+    yml_str = (header+body+marker_string).replace("'","")# remove '
+    with open("saved.yml",'w') as file:
+        # doc = yaml.dump(config,file)
+        file.write(yml_str)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--path',help='marker set yaml to be centered',default="markerset.yml")
+    parser.add_argument('--gui',help='visualize the marker map',type=bool,default=True)
+
+    args = parser.parse_args()
+    arucoTest(args.path, args.gui)
